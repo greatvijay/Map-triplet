7ab5571da50a5d5df4b15378b07f0ab7276ec8cc
greatvijay <136738331+greatvijay@users.noreply.github.com>
2023-07-21 23:47:03 +0530

    Add files via upload

---
+++ maptriplet.py
@@ -0,0 +1,5 @@
+sample_list = [1, 2, 3, 4, 5,6,7] # Sample list of integer
+tripled_numbers = list(map(lambda x: x * 3, sample_list))#Using map function
+
+print("Original list:", sample_list)
+print("Tripled list:", tripled_numbers)
